dc64bb5f2389e8b7c56e0720e7f33c0238667881
basma387 <basmayasser022@gmail.com>
2025-04-11 22:51:34 +0200

    Update Assignment8.sol

--- src/Assignment8.sol
+++ src/Assignment8.sol
@@ -1,22 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";    
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-// Define the custom error
-error OwnableUnauthorizedAccount(address account);
-
-// Make this contract inherit from ERC721URIStorage and Ownable
 contract Assignment8 is ERC721URIStorage, Ownable {
-    // Declare a private uint called _tokenIdCounter
     uint256 private _tokenIdCounter;
 
-    // Pass name and symbol of the NFT token collection
-    constructor() ERC721("BuzzBuzz", "Buz") {}
+    constructor() ERC721("BuzzBuzz", "Buz") Ownable(msg.sender) {}
 
-    // Mint NFT function
     function mintNFT(string memory jsonURI) external onlyOwner returns (uint256) {
         uint256 newTokenId = _tokenIdCounter;
 
@@ -28,10 +14,10 @@ contract Assignment8 is ERC721URIStorage, Ownable {
         return newTokenId;
     }
 
-    // Use a custom function instead of overriding onlyOwner
     function enforceOnlyOwner() internal view {
         if (msg.sender != owner()) {
             revert OwnableUnauthorizedAccount(msg.sender);
         }
     }
 }
+       
